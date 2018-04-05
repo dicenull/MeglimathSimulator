3157f16fcd6bc9f37125dfd1a24db98d0e526fbd
dice <dicenull.dev@gmail.com>
2018-04-05 18:54:46 +0900

    フィールド内のセルを描画できるようにした

--- MegurimasuSimulator/Field.cpp
+++ MegurimasuSimulator/Field.cpp
@@ -1,11 +1,41 @@
 #include "Field.h"
 
+void Field::Draw() const
+{
+	Rect r;
+	for (int i = 0; i < _cells.size().x; i++)
+	{
+		for (int k = 0; k < _cells.size().y; k++)
+		{
+			r = Rect(_origin + Point(i, k) * _c_size, _c_size);
 
+			r.draw(Tile::ColorOf(_cells[k][i].GetTile()));
+			r.drawFrame(1.0, Palette::Gray);
+
+			_font(_cells[k][i].GetPoint()).drawAt(r.center(), Palette::Black);
+		}
+	}
+}
 
 Field::Field()
 {
 }
 
+void Field::operator=(const Field & other)
+{
+	_cells = other._cells;
+	_origin = other._origin;
+}
+
+
+Field::Field(Point origin, Size size)
+{
+	_origin = origin;
+	_cells = Grid<Cell>(size);
+}
+
+Field::Field(Size size)
+	:Field(Point(0, 0), size){}
 
 Field::~Field()
 {
--- MegurimasuSimulator/Field.h
+++ MegurimasuSimulator/Field.h
@@ -5,8 +5,31 @@ class Field
 {
 private:
 	Grid<Cell> _cells;
+	Point _origin;
+
+	/// <summary>
+	/// セル一つ当たりの大きさ
+	/// </summary>
+	const Size _c_size = Size(32, 32);
+
+	const Font _font{ _c_size.x / 2 };
+
+public:
+	void Draw() const;
+
 
 public:
 	Field();
+
+	void operator=(const Field& other);
+
+	/// <summary>
+	/// コンストラクタ
+	/// </summary>
+	/// <param name="origin">原点</param>
+	/// <param name="size">フィールドの大きさ</param>
+	Field(Point origin, Size size);
+
+	Field(Size size);
 	virtual ~Field();
 };
