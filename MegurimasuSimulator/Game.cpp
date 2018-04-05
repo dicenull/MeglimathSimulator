e643c5d7fd833a4fa94518ff4c67f52709ded53d
dice <dicenull.dev@gmail.com>
2018-04-05 18:55:08 +0900

    ゲームを管理するクラスを追加

--- MegurimasuSimulator/Game.cpp
+++ MegurimasuSimulator/Game.cpp
@@ -1,9 +1,21 @@
 #include "Game.h"
 
 
-
-Game::Game()
+void Game::Update()
 {
+
+}
+
+void Game::Draw()
+{
+	_field.Draw();
+}
+
+Game::Game(const Field &field, Team team_a, Team team_b)
+{
+	_field = field;
+	_teams[0] = team_a;
+	_teams[1] = team_b;
 }
 
 
--- MegurimasuSimulator/Game.h
+++ MegurimasuSimulator/Game.h
@@ -2,14 +2,18 @@
 #include "Field.h"
 #include "Team.h"
 
-class Game
-{
-private:
-	Field _field;
-	Team _teams[2];
 
-public:
-	Game();
-	virtual ~Game();
-};
+	class Game
+	{
+	private:
+		Field _field;
+		Team _teams[2];
 
+	public:
+		void Update();
+		void Draw();
+
+	public:
+		Game(const Field &field, Team team_a, Team team_b);
+		virtual ~Game();
+	};
