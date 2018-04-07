83607a02d726b43eac6d3cc547ae065d060ce41f
dice <dicenull.dev@gmail.com>
2018-04-07 21:37:42 +0900

    チームから次の行動を送るように変更

--- MegurimasuSimulator/Team.cpp
+++ MegurimasuSimulator/Team.cpp
@@ -1,11 +1,31 @@
 #include "Team.h"
 
-
-
-Team::Team()
+Think Team::NextThink(GameInfo info)
 {
+	return Think({ Step{ Action::Stop }, Step{ Action::Stop } });
 }
 
+Array<Agent> Team::GetAgents()
+{
+	return Array<Agent>({ _agents[0], _agents[1] });
+}
+
+void Team::MoveAgent(int idx, Direction dir)
+{
+	_agents[idx].Move(dir);
+}
+
+Team::Team()
+	:Team(TeamType::A, Agent(), Agent())
+{}
+
+Team::Team(TeamType type, Agent agent1, Agent agent2)
+{
+	_agents[0] = agent1;
+	_agents[1] = agent2;
+	_type = type;
+	_total_point = 0;
+}
 
 Team::~Team()
 {
--- MegurimasuSimulator/Team.h
+++ MegurimasuSimulator/Team.h
@@ -1,16 +1,38 @@
 #pragma once
-#include "ControlTower.h"
+#include "GameInfo.h"
+#include "Think.h"
+#include "TeamType.h"
 #include "Agent.h"
 
 class Team
 {
 private:
 	int _total_point;
-	ControlTower _control_tower;
 	Agent _agents[2];
+	TeamType _type;
+
+public:
+	virtual Think NextThink(GameInfo info);
+
+	Array<Agent> GetAgents();
+	
+	/// <summary>
+	/// 指定のエージェントを動かす
+	/// </summary>
+	/// <param name="idx">エージェントの番号</param>
+	/// <param name="dir">動かす方向</param>
+	void MoveAgent(int idx, Direction dir);
 
 public:
 	Team();
+
+	/// <summary>
+	/// コンストラクタ
+	/// </summary>
+	/// <param name="type">チームの種類</param>
+	/// <param name="agent1">一人目のエージェント</param>
+	/// <param name="agent2">二人目のエージェント</param>
+	Team(TeamType type, Agent agent1, Agent agent2);
 	virtual ~Team();
 };
 
