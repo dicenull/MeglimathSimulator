44e5ea542e4b8f19c413ae570e548ec7636f9e3e
dice <dicenull.dev@gmail.com>
2018-04-10 00:53:29 +0900

    RandomでThinkを返すチームを追加

---
+++ MegurimasuSimulator/RandomTeam.cpp
@@ -0,0 +1,5 @@
+#include "RandomTeam.h"
+
+RandomTeam::RandomTeam(TeamType type, Agent agent1, Agent agent2)
+	:Team(type, agent1, agent2)
+{}
---
+++ MegurimasuSimulator/RandomTeam.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <Siv3D.hpp>
+#include "Team.h"
+
+class  RandomTeam : public Team
+{
+public:
+	Think NextThink(GameInfo info) override
+	{
+		Action action = static_cast<Action>(Random(0, 1));
+		Direction dir = static_cast<Direction>(Random(0, 7));
+		Step step{ action, dir};
+
+		return Think{ step, step };
+	}
+
+public:
+	RandomTeam(TeamType type, Agent agent1, Agent agent2);
+};
