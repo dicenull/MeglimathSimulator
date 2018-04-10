bdcd58f0019a5a47cdc57b92632377cc8ba134f4
Eulerd <dicenull.dev@gmail.com>
2018-04-10 17:08:14 +0900

    エージェントの描画方法を変更

--- MegurimasuSimulator/FieldDrawer.cpp
+++ MegurimasuSimulator/FieldDrawer.cpp
@@ -25,24 +25,42 @@ void FieldDrawer::Draw(const GameInfo & info) const
 		for (int k = 0; k < cells.size().y; k++)
 		{
 			Point pos(i, k);
-			r = Rect(_origin + pos * _c_size, _c_size);
+			Point top_left_pos = _origin + pos * _c_size;
+			Point center_pos = top_left_pos + _c_size / 2;
+			int32 edge_width = Sqrt(2) * _c_size.x / 2.0;
+
+			r = Rect(top_left_pos, _c_size);
 
 			r.draw(Transform::ColorOf(cells[k][i].GetTile()));
 
 			// エージェントがいる場合枠の色を変える
-			Color frame_color = Palette::Gray;
 			auto pos_lamda = [pos](const Agent & agent) {return agent.GetPosition() == pos; };
 
-			if (a_agents.includes_if(pos_lamda))
+			r.drawFrame(1.0, Palette::Gray);
+
+			// チームAのエージェントを描画
+			if (pos == a_agents[0].GetPosition())
 			{
-				frame_color = Palette::Magenta;
+				Circle(center_pos, _c_size.x / 2).draw(Palette::Red);
 			}
-			else if (b_agents.includes_if(pos_lamda))
+			if (pos == a_agents[1].GetPosition())
 			{
-				frame_color = Palette::Cyan;
+				// ひし形
+				Rect(Arg::center = center_pos, edge_width).rotated(45_deg)
+					.draw(Palette::Red);
 			}
 
-			r.drawFrame(2.0, 0, frame_color);
+			// チームBのエージェントを描画
+			if (pos == b_agents[0].GetPosition())
+			{
+				Circle(center_pos, _c_size.x / 2).draw(Palette::Blue);
+			}
+			if (pos == b_agents[1].GetPosition())
+			{
+				// ひし形
+				Rect(Arg::center = center_pos, edge_width).rotated(45_deg)
+					.draw(Palette::Blue);
+			}
 
 			FontAsset(U"Cell")(cells[k][i].GetPoint()).drawAt(r.center(), Palette::Black);
 		}
