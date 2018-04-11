e6983bcfef78bbe02d0d06d00aa3156c20531a1c
dice <dicenull.dev@gmail.com>
2018-04-11 20:30:04 +0900

    jsonファイルからフィールドを生成する関数を追加

--- MegurimasuSimulator/Field.cpp
+++ MegurimasuSimulator/Field.cpp
@@ -1,5 +1,34 @@
 #include "Field.h"
 
+Field Field::Create(FilePath file)
+{
+	JSONReader json(file);
+
+	Size size = json[U"Size"].get<Size>();
+	auto points = json[U"Points"].arrayView();
+
+	// 入力されるタイルポイントの数
+	Size data_size = Size((size.x + 1) / 2, (size.y + 1) / 2);
+
+	Grid<Cell> cells = Grid<Cell>(size);
+
+	// タイルポイントをグリッド状に成型して入力
+	int idx = 0;
+	for (int i : step(data_size.x))
+	{
+		for (int k : step(data_size.y))
+		{
+			cells[k][i] = points[idx].get<int>();
+			// データをコピー
+			cells[size.y - 1 - k][size.x - 1 - i] = cells[k][i];
+
+			idx++;
+		}
+	}
+
+	return Field(cells);
+}
+
 Grid<Cell> Field::GetCells() const
 {
 	return _cells;
