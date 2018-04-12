fa2ac2bef696be8f0a6ec939404e6cd3eefe2965
Eulerd <dicenull.dev@gmail.com>
2018-04-12 17:47:44 +0900

    データの参照順が間違っていたので修正。 タイル情報があった場合読み込むようにした

--- MegurimasuSimulator/Field.cpp
+++ MegurimasuSimulator/Field.cpp
@@ -14,15 +14,41 @@ Field Field::Create(FilePath file)
 
 	// タイルポイントをグリッド状に成型して入力
 	int idx = 0;
-	for (int i : step(data_size.x))
+	for (int i : step(data_size.y))
 	{
-		for (int k : step(data_size.y))
+		for (int k : step(data_size.x))
 		{
 			cells[k][i] = points[idx].get<int>();
 			// データをコピー
 			cells[size.y - 1 - k][size.x - 1 - i] = cells[k][i];
 
 			idx++;
+		}
+	}
+
+	if (json[U"Tiles"].isEmpty())
+	{
+		return Field(cells);
+	}
+
+	// テスト用にタイル情報がある場合読み込んで入力する
+	auto tiles = json[U"Tiles"].arrayView();
+	
+	for (int i : step(size.y))
+	{
+		for (int k : step(size.x))
+		{
+			switch (tiles[i].getString()[k])
+			{
+			case 'a':
+				cells[k][i].PaintedBy(TeamType::A);
+				break;
+			case 'b':
+				cells[k][i].PaintedBy(TeamType::B);
+				break;
+			default:
+				break;
+			}
 		}
 	}
 
