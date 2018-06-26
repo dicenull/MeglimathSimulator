bb7d86b19cbf90926fcf7d5f3d93c16ed4ce41cb
bamboowonsstring <extra.excramattion1@gmail.com>
2018-06-26 18:06:28 +0900

    std::iotaを使用

--- MeglimathCore/GameLogic/Util.h
+++ MeglimathCore/GameLogic/Util.h
@@ -1,7 +1,16 @@
 #pragma once
 #include<vector>
-static std::vector<int> step(size_t n) {
-	std::vector<int> ret;
-	for (int i = 0; i < n; i++)ret.push_back(i);
-	return ret;
+#include<array>
+
+template<int n>
+inline const std::array<int, n> &step() {
+	auto a = std::array<int, n>();
+	std::iota(a.begin(), a.end());
+	return a;
+}
+
+inline const std::vector<int> &step(size_t n) {
+	auto v = std::vector<int>(n);
+	std::iota(v.begin(), v.end(), n);
+	return v;
 }
