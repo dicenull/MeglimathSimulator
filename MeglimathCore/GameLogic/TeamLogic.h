418bde39782b510a54c15276ebd3a15978e35f5d
bamboowonsstring <extra.excramattion1@gmail.com>
2018-09-02 01:39:56 +0900

    classを整理

--- MeglimathCore/GameLogic/TeamLogic.h
+++ MeglimathCore/GameLogic/TeamLogic.h
@@ -2,55 +2,56 @@
 #include "Types.h"
 #include "Agent.h"
 #include <array>
-class TeamLogic
-{
-private:
+struct TeamLogic
+{	
+	// フィールド //
+	/// <summary>
+	/// チームA,Bを判断するためのチームの種類
+	/// </summary>
+	TeamType type;
+
 	/// <summary>
 	/// チームに所属するエージェント
 	/// </summary>
 	std::array<Agent,2> agents;
 
-	/// <summary>
-	/// チームA,Bを判断するためのチームの種類
-	/// </summary>
-	TeamType _type;
-
-public:
+	// メソッド //
 	/// <summary>
 	/// エージェントの初期位置を初期化する
 	/// </summary>
 	/// <param name="pos1">エージェント1の初期座標</param>
 	/// <param name="pos2">エージェント2の初期座標</param>
-	void InitAgentsPos(_Point<> pos1, _Point <>pos2);
-
-	const std::array<Agent, 2>& GetAgents() const;
+	void InitAgentsPos(_Point<> pos1, _Point<> pos2)
+	{
+		agents = { { { pos1 },{ pos2 } } };
+	}
 
 	/// <summary>
 	/// 指定のエージェントを動かす
 	/// </summary>
 	/// <param name="idx">エージェントの番号</param>
 	/// <param name="dir">動かす方向</param>
-	void MoveAgent(int idx, Direction dir);
+	void MoveAgent(int idx, Direction dir)
+	{
+		agents[idx].Move(dir);
+	}
 
 	/// <summary>
 	/// 指定座標にいるエージェントを動かす
 	/// </summary>
 	/// <param name="pos">エージェントのいる座標</param>
 	/// <param name="dir">動かす方向</param>
-	void MoveAgent(_Point<> pos, Direction dir);
-	TeamType getTeamType() const;
-public:
-	TeamLogic();
+	void MoveAgent(_Point<> pos, Direction dir)
+	{
+		for (auto & agent : agents)
+		{
+			if (agent.position == pos)
+			{
+				agent.Move(dir);
+				return;
+			}
+		}
+	}
 
-	/// <summary>
-	/// コンストラクタ
-	/// </summary>
-	/// <param name="type">チームの種類</param>
-	/// <param name="agent1">一人目のエージェント</param>
-	/// <param name="agent2">二人目のエージェント</param>
-	TeamLogic(TeamType type, Agent agent1, Agent agent2);
-
-	//コピーコンストラクタ
-	TeamLogic(const TeamLogic &tl);
 };
 
