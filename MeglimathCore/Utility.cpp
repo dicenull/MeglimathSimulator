a8459e5bd4d47f3cff3cc54725a8451e3f260eaf
dice <dicenull.dev@gmail.com>
2018-09-28 22:01:45 +0900

    AllStepの追加

---
+++ MeglimathCore/Utility.cpp
@@ -0,0 +1,23 @@
+﻿#include "Utility.h"
+
+namespace Utility
+{
+	const std::array<Step, 16> Utility::AllStep()
+	{
+		std::array<Step, 16> all_step = {};
+
+		int index = 0;
+		for (auto act : { Action::Move, Action::RemoveTile })
+		{
+			for (auto i = 0; i < 8; i++)
+			{
+				auto dir = static_cast<Direction>(i);
+				all_step[index] = { act, dir };
+
+				index++;
+			}
+		}
+
+		return all_step;
+	}
+}
--- MeglimathCore/Utility.h
+++ MeglimathCore/Utility.h
@@ -4,24 +4,6 @@
 
 namespace Utility
 {
-	const std::array<Step, 16> AllStep()
-	{
-		std::array<Step, 16> all_step;
-
-		int index = 0;
-		for (auto act : { Action::Move, Action::RemoveTile })
-		{
-			for (auto i = 0; i < 7; i++)
-			{
-				auto dir = static_cast<Direction>(i);
-				all_step[index] = { act, dir };
-
-				index++;
-			}
-		}
-
-		return all_step;
-	}
-
+	const std::array<Step, 16> AllStep();
 
 }
